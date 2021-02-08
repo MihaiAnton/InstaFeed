f80a9d39fd4773969f8bc14dac75d5095d18baec
Mihai Anton <antonmihai58@gmail.com>
2021-02-08 10:39:46 +0200

    Adds celery

--- instafeed/settings.py
+++ instafeed/settings.py
@@ -144,25 +144,25 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-# # CELERY STUFF
-# BROKER_URL = 'redis://redis:6379'
-# CELERY_RESULT_BACKEND = 'redis://redis:6379'
-# CELERY_ACCEPT_CONTENT = ['application/json']
-# CELERY_TASK_SERIALIZER = 'json'
-# CELERY_RESULT_SERIALIZER = 'json'
-# CELERY_TIMEZONE = 'Europe/London'
-# CELERY_BEAT_SCHEDULE = {
-#     'refresh_profile_knowledge': {
-#         'task': 'instagramscraper.tasks.scrap_and_check_for_updates',
-#         'schedule': 14400,
-#         'args': (),
-#     },
-#     'send_daily_update_mail': {
-#         'task': 'instagramscraper.tasks.send_daily_updates_email',
-#         'schedule': crontab(hour=18, minute=00),
-#         'args': (),
-#     }
-# }
+# CELERY STUFF
+BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Europe/London'
+CELERY_BEAT_SCHEDULE = {
+    'refresh_profile_knowledge': {
+        'task': 'instagramscraper.tasks.scrap_and_check_for_updates',
+        'schedule': 14400,
+        'args': (),
+    },
+    'send_daily_update_mail': {
+        'task': 'instagramscraper.tasks.send_daily_updates_email',
+        'schedule': crontab(hour=18, minute=00),
+        'args': (),
+    }
+}
 
 STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
